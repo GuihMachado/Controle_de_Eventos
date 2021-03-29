70c68823717ee22ecd0b5071d140e99cdc4c7980
gui.machado <guilhermemachadobr0@gmail.com>
2021-03-29 00:11:42 -0300

    Insert

--- src/main/java/com/example/system/controllers/EventosController.java
+++ src/main/java/com/example/system/controllers/EventosController.java
@@ -1,10 +1,18 @@
 package com.example.system.controllers;
 
+import java.net.URI;
+
+import com.example.system.dtos.EventosDTO;
+import com.example.system.dtos.EventosInsertDTO;
 import com.example.system.services.EventosService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/eventos")
@@ -13,5 +21,11 @@ public class EventosController {
     @Autowired
     private EventosService service;
 
+    @PostMapping
+	public ResponseEntity<EventosDTO> insert(@RequestBody EventosInsertDTO insertDto){
+		EventosDTO dto = service.insert(insertDto); 
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
 
 }
--- src/main/java/com/example/system/services/EventosService.java
+++ src/main/java/com/example/system/services/EventosService.java
@@ -1,5 +1,8 @@
 package com.example.system.services;
 
+import com.example.system.dtos.EventosDTO;
+import com.example.system.dtos.EventosInsertDTO;
+import com.example.system.entities.Eventos;
 import com.example.system.repositories.EventosRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,5 +14,10 @@ public class EventosService {
     @Autowired
     private EventosRepository repo;
 
+    public EventosDTO insert(EventosInsertDTO dto){
+        Eventos entity = new Eventos(dto);
+        entity = repo.save(entity);
+        return new EventosDTO(entity);
+    }
     
 }
